0eb0c1fe6a393ea461b4310b3a5ee6f64230f2c5
Paul Bender <pebender@gmail.com>
2018-12-17 12:32:00 -0800

    Added file missed in prior commit.

--- alexa-lg-webos-tv-controller/index.js
+++ alexa-lg-webos-tv-controller/index.js
@@ -1,11 +1,6 @@
-const smartHomeSkill = require('./lib/smart-home/index.js');
-const customSkill = require('./lib/custom/index.js');
+const customSkill = require('./lib/custom-skill/index.js');
 
 function skilllHandler(event, context, callback) {
-    const smartHomeSkillHandler = smartHomeSkill.getHandler(event, context);
-    if (typeof smartHomeSkillHandler === 'function') {
-        return smartHomeSkillHandler(event, context, callback);
-    }
     return customSkill.handler(event, context, callback);
 }
 
