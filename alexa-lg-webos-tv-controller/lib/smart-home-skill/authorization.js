8447869236b0a5201a9dc6646d5ff5f0842e3b7e
Paul Bender <pebender@gmail.com>
2019-03-09 17:28:20 -0800

    Converted one more controller handler to Promises.

--- alexa-lg-webos-tv-controller/lib/smart-home-skill/authorization.js
+++ alexa-lg-webos-tv-controller/lib/smart-home-skill/authorization.js
@@ -1,5 +1,8 @@
-function handler(event, callback) {
-    callback(null, null);
+// eslint-disable-next-line no-unused-vars
+function handler(_event) {
+    return new Promise((resolve) => {
+        resolve(null);
+    });
 }
 
 module.exports = {
