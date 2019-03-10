d90c056f4e124c8d203b3dd66bc9f522c5a29bbe
Paul Bender <pebender@gmail.com>
2019-03-10 10:56:08 -0700

    Converted some of controller's gateway-api functions to async/await.

--- alexa-lg-webos-tv-controller/lib/gateway-api.js
+++ alexa-lg-webos-tv-controller/lib/gateway-api.js
@@ -38,70 +38,34 @@ class Gateway {
 
     // eslint-disable-next-line class-methods-use-this
     sendSkillDirective(request) {
-        return new Promise((resolve, reject) => {
-            const options = {
-                "hostname": this.hostname,
-                "username": this.username,
-                "password": this.password,
-                "path": "/LGTV/SKILL"
-            };
-            sendHandler(options, request).then((response) => resolve(response), (error) => reject(error));
-        });
+        const options = {
+            "hostname": this.hostname,
+            "username": this.username,
+            "password": this.password,
+            "path": "/LGTV/SKILL"
+        };
+        return sendHandler(options, request);
     }
 
     ping() {
-        return new Promise((resolve, reject) => {
-            const options = {
-                "hostname": this.hostname,
-                "username": this.username,
-                "password": this.password,
-                "path": "/LGTV/PING"
-            };
-            pingHandler(options).then((response) => resolve(response), (error) => reject(error));
-        });
+        const options = {
+            "hostname": this.hostname,
+            "username": this.username,
+            "password": this.password,
+            "path": "/LGTV/PING"
+        };
+        return pingHandler(options);
     }
 }
 
 function pingHandler(requestOptions) {
     return new Promise((resolve, reject) => {
-
-        if (Reflect.has(requestOptions, "hostname") === false || requestOptions.hostname === null) {
-            const error = new Error();
-            error.name = "HOSTNAME_NOT_SET";
-            error.message = "The gateway hostname has not been set.";
+        let options = null;
+        try {
+            options = createBasicOptions(requestOptions);
+        } catch (error) {
             reject(error);
         }
-
-        if (Reflect.has(requestOptions, "username") === false || requestOptions.username === null) {
-            const error = new Error();
-            error.name = "USERNAME_NOT_SET";
-            error.message = "The gateway username has not been set.";
-            reject(error);
-        }
-
-        if (Reflect.has(requestOptions, "password") === false || requestOptions.password === null) {
-            const error = new Error();
-            error.name = "PASSWORD_NOT_SET";
-            error.message = "The gateway password has not been set.";
-            reject(error);
-        }
-
-        if (Reflect.has(requestOptions, "path") === false || requestOptions.path === null) {
-            const error = new Error();
-            error.name = "PATH_NOT_SET";
-            error.message = "The gateway path has not been set.";
-            reject(error);
-        }
-
-        const authorization = Buffer.from(`${requestOptions.username}:${requestOptions.password}`).toString("base64");
-        const options = {
-            "hostname": requestOptions.hostname,
-            "port": 25392,
-            "path": requestOptions.path,
-            "headers": {
-                "Authorization": `Basic ${authorization}`
-            }
-        };
         const request = https.get(options);
         request.once("response", (response) => {
             response.setEncoding("utf8");
@@ -134,52 +98,16 @@ function pingHandler(requestOptions) {
 
 function sendHandler(requestOptions, requestBody) {
     return new Promise((resolve, reject) => {
-
-        if (Reflect.has(requestOptions, "hostname") === false || requestOptions.hostname === null) {
-            const error = new Error();
-            error.name = "HOSTNAME_NOT_SET";
-            error.message = "The gateway hostname has not been set.";
+        let options = null;
+        try {
+            options = createBasicOptions(requestOptions);
+        } catch (error) {
             reject(error);
         }
-
-        if (Reflect.has(requestOptions, "username") === false || requestOptions.username === null) {
-            const error = new Error();
-            error.name = "USERNAME_NOT_SET";
-            error.message = "The gateway username has not been set.";
-            reject(error);
-        }
-
-        if (Reflect.has(requestOptions, "password") === false || requestOptions.password === null) {
-            const error = new Error();
-            error.name = "PASSWORD_NOT_SET";
-            error.message = "The gateway password has not been set.";
-            reject(error);
-        }
-
-        if (Reflect.has(requestOptions, "path") === false || requestOptions.path === null) {
-            const error = new Error();
-            error.name = "PATH_NOT_SET";
-            error.message = "The gateway path has not been set.";
-            reject(error);
-        }
-
-        const authorization = Buffer.from(`${requestOptions.username}:${requestOptions.password}`).toString("base64");
         const content = JSON.stringify(requestBody);
-        const options = {
-            "hostname": requestOptions.hostname,
-            "port": 25392,
-            "path": requestOptions.path,
-            "method": "POST",
-            "headers": {
-                "Authorization": `Basic ${authorization}`,
-                "Content-Type": "application/json",
-                "Content-Length": Buffer.byteLength(content)
-            },
-            "rejectUnauthorized":
-                Reflect.has(requestOptions, "rejectUnauthorized")
-                    ? requestOptions.rejectUnauthorized
-                    : true
-        };
+        options.method = "POST";
+        options.headers["Content-Type"] = "application/json";
+        options.headers["Content-Length"] = Buffer.byteLength(content);
         const request = https.request(options);
         request.once("response", (response) => {
             let body = {};
@@ -236,20 +164,48 @@ function sendHandler(requestOptions, requestBody) {
     });
 }
 
-/*
-class GatewayAPIError extends Error {
-    constructor(message, ...args) {
+function createBasicOptions(requestOptions) {
+    if (Reflect.has(requestOptions, "hostname") === false || requestOptions.hostname === null) {
+        throw new GenericError("HOSTNAME_NOT_SET", "The gateway hostname has not been set.");
+    }
+    if (Reflect.has(requestOptions, "username") === false || requestOptions.username === null) {
+        throw new GenericError("USERNAME_NOT_SET", "The gateway username has not been set.");
+    }
+    if (Reflect.has(requestOptions, "password") === false || requestOptions.password === null) {
+        throw new GenericError("PASSWORD_NOT_SET", "The gateway password has not been set.");
+    }
+    if (Reflect.has(requestOptions, "path") === false || requestOptions.path === null) {
+        throw new GenericError("PATH_NOT_SET", "The gateway path has not been set.");
+    }
+
+    const authorization = Buffer.from(`${requestOptions.username}:${requestOptions.password}`).toString("base64");
+    const options = {
+        "hostname": requestOptions.hostname,
+        "port": 25392,
+        "path": requestOptions.path,
+        "headers": {
+            "Authorization": `Basic ${authorization}`
+        },
+        "rejectUnauthorized":
+            Reflect.has(requestOptions, "rejectUnauthorized")
+                ? requestOptions.rejectUnauthorized
+                : true
+    };
+    return options;
+}
+
+
+class GenericError extends Error {
+    constructor(name, message, ...args) {
         super(message, ...args);
-        this.name = "GatewayAPI_Error";
-        this.message = message;
+        this.name = name;
+        Error.captureStackTrace(this, this.constructor);
     }
 }
-*/
 
-class GatewayAPIError extends Error {
-    // eslint-disable-next-line no-useless-constructor
+class GatewayAPIError extends GenericError {
     constructor(message, ...args) {
-        super(message, ...args);
+        super("GatewayAPIError", message, ...args);
     }
 }
 
