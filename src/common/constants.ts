51d2c275c761b85494eb6190c049c76a57134d33
Paul Bender <pebender@gmail.com>
2022-04-05 14:24:21 -0700

    Updated constants.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 export enum constants {
   'amazonVendorId' = 'M2V6930QON4TLV',
-  'gatewayHostname' = 'alexa.backinthirty.net',
+  'gatewayHostname' = 'home.backinthirty.net',
   'gatewayRootPassword' = 'd86566e397a9afb420fee18c5387b4de032a1fe402e14be2c76e91cf13554d870b57fcfc1030e424fb12324b9ca64324c79dbc6079bbd19086779fdbc31cbf81',
-  'gatewayUserPassword' = '37f0098320b4478980ceee37acdc208bd830127bfc51d2136c274f280852a58b70b2d9351266a68492cb4284c99869a417866ec5dd0d02ac1788d8ab0295c0c6'
+  'gatewayUserPassword' = '09d1638b516783c85a18386532a87f629f5d2dce4b1a2abc363aca3c258433589bfac2f5f98699d1f44af00b05099e320813a1b59a50a5dfb92c5ccaa51bb914'
 }
