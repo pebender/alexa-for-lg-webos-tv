7d130ac7cbab7c72563dd4632cc090ee8e5d6962
Paul Bender <pebender@gmail.com>
2022-04-08 14:24:45 -0700

    Switched to using nedb's promise interface.

--- src/bridge/lib/database.ts
+++ src/bridge/lib/database.ts
@@ -27,139 +27,58 @@ export class DatabaseTable extends BaseClass {
     // webOS TV bridge can run.
     //
     this._db = new Datastore({ filename: `${path}/${name}.nedb` })
-    this._db.loadDatabase((error): void => {
-      if (error) {
-        throw error
-      }
-    })
+    this._db.loadDatabaseAsync()
   }
 
   public initialize (): Promise<void> {
     const that = this
+
     function initializeFunction (): Promise<void> {
-      return new Promise<void>((resolve): void => {
-        that._indexes.forEach((record): void => {
-          that._db.ensureIndex({
-            fieldName: record,
-            unique: true
-          })
-        })
-        resolve()
-      })
+      function index (fieldName: string): Promise<void> {
+        return that._db.ensureIndexAsync({ fieldName, unique: true })
+      }
+      return Promise.all(that._indexes.map(index)).then()
     }
+
     return this.initializeHandler(initializeFunction)
   }
 
   public async clean (): Promise<void> {
     this.throwIfUninitialized('clean')
-    const that = this
     const query1: DatabaseQuery = {}
     query1[this._key] = { $exists: false }
     const query2: DatabaseQuery = {}
     query2[this._key] = null
     const query: DatabaseQuery = { $or: [query1, query2] }
-    await new Promise<void>((resolve, reject): void => {
-      that._db.remove(
-        query,
-        { multi: true },
-        (error): void => {
-          if (error) {
-            reject(error)
-            return
-          }
-          resolve()
-        }
-      )
-    })
+    await this._db.removeAsync(query, { multi: true })
+    await this._db.compactDatafileAsync()
   }
 
   public async getRecord (query: DatabaseQuery): Promise<DatabaseRecord> {
     this.throwIfUninitialized('getRecord')
-    const that = this
-    const record = await new Promise<DatabaseRecord>((resolve, reject): void => {
-      that._db.findOne(
-        query,
-        (err, doc: DatabaseRecord): void => {
-          if (err) {
-            reject(err)
-            return
-          }
-          resolve(doc)
-        }
-      )
-    })
-    return record
+    return await this._db.findOneAsync<DatabaseRecord>(query).execAsync()
   }
 
   public async getRecords (query: DatabaseQuery): Promise<DatabaseRecord[]> {
     this.throwIfUninitialized('getRecords')
-    const that = this
-    const records = await new Promise<DatabaseRecord[]>((resolve, reject): void => {
-      that._db.find(
-        query,
-        (error: Error, docs: DatabaseRecord[]): void => {
-          if (error) {
-            reject(error)
-            return
-          }
-          resolve(docs)
-        }
-      )
-    })
-    return records
+    return await this._db.findAsync<DatabaseRecord>(query).execAsync()
   }
 
   public async insertRecord (record: DatabaseRecord): Promise<void> {
     this.throwIfUninitialized('insertRecord')
-    const that = this
-    await new Promise<void>((resolve, reject): void => {
-      that._db.insert(record, (error): void => {
-        if (error) {
-          reject(error)
-          return
-        }
-        resolve()
-        // eslint-disable-next-line no-useless-return
-        return
-      })
-    })
+    await this._db.insertAsync(record)
+    await this._db.compactDatafileAsync()
   }
 
   public async updateRecord (query: DatabaseQuery, update: DatabaseUpdate): Promise<void> {
     this.throwIfUninitialized('updateRecord')
-    const that = this
-    await new Promise<void>((resolve, reject): void => {
-      that._db.update(
-        query,
-        update,
-        {},
-        (error): void => {
-          if (error) {
-            reject(error)
-            return
-          }
-          resolve()
-        }
-      )
-    })
+    await this._db.updateAsync(query, update, {})
+    await this._db.compactDatafileAsync()
   }
 
   public async updateOrInsertRecord (query: DatabaseQuery, update: DatabaseUpdate): Promise<void> {
     this.throwIfUninitialized('updateOrInsertRecord')
-    const that = this
-    await new Promise<void>((resolve, reject): void => {
-      that._db.update(
-        query,
-        update,
-        { upsert: true },
-        (error): void => {
-          if (error) {
-            reject(error)
-            return
-          }
-          resolve()
-        }
-      )
-    })
+    await this._db.updateAsync(query, update, { upsert: true })
+    await this._db.compactDatafileAsync()
   }
 }
