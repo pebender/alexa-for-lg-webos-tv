cd26c61a6405f98216fe3b5ba38d8aff807ee003
Paul Bender <pebender@gmail.com>
2022-04-25 11:21:33 -0700

    Moved more CS slot validation to the interaction model.

--- src/skill/lib/custom-skill/lgtv-configure-bridge.ts
+++ src/skill/lib/custom-skill/lgtv-configure-bridge.ts
@@ -24,6 +24,37 @@ function getHostnames (ipAddress: string, ipPort: number): Promise<string[]> {
   })
 }
 
+async function creatHostnamesSimpleCardContent (handlerInput: ASKHandlerInput): Promise<string> {
+  const sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
+  const ipAddressA: Number = Number(ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressA'))
+  const ipAddressB: Number = Number(ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressB'))
+  const ipAddressC: Number = Number(ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressC'))
+  const ipAddressD: Number = Number(ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressD'))
+  sessionAttributes.ipAddress = `${ipAddressA}.${ipAddressB}.${ipAddressC}.${ipAddressD}`
+  Reflect.deleteProperty(sessionAttributes, 'hostnames')
+  handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
+  try {
+    sessionAttributes.hostnames = await getHostnames(sessionAttributes.ipAddress, 25392)
+  } catch (error) {
+    Common.Debug.debug(`LGTV_ConfigureBridgeIntent: cannot connect to IPv4 address '${sessionAttributes.ipAddress}'.`)
+    Common.Debug.debugError(error)
+    throw new Error('I had a problem connecting to the bridge\'s I.P.  address.')
+  }
+  handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
+  Common.Debug.debug(`LGTV_ConfigureBridgeIntent: bridge FQDNs: ${sessionAttributes.hostnames}`)
+  let cardContent: string = ''
+  let index = 0
+  while (index < sessionAttributes.hostnames.length) {
+    cardContent += `${index}: ${sessionAttributes.hostnames[index]}\n`
+    index += 1
+  }
+  cardContent += `\n${index}: My bridge is not in the list of hostnames.`
+  index += 1
+  cardContent += `\n${index}: My IP address is not '${sessionAttributes.ipAddress}'.`
+  Common.Debug.debug(`LGTV_ConfigureBridgeIntent: bridge FQDN prompt: ${cardContent}`)
+  return cardContent
+}
+
 const ConfigureBridgeIntentHandler = {
   canHandle (handlerInput: ASKHandlerInput): boolean {
     return ASKRequestEnvelope.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
@@ -33,34 +64,7 @@ const ConfigureBridgeIntentHandler = {
     Common.Debug.debugJSON(handlerInput.requestEnvelope)
 
     const sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
-
-    if (typeof handlerInput.requestEnvelope.request === 'undefined') {
-      const errorMessage = 'LGTV_ConfigureBridgeIntent: invalid code path: request undefined'
-      const error = new Error(errorMessage)
-      Common.Debug.debugErrorWithStack(error)
-      throw error
-    }
-
     const intentRequest = (handlerInput.requestEnvelope.request as ASKModel.IntentRequest)
-
-    if (typeof intentRequest.intent.slots === 'undefined') {
-      const errorMessage = 'LGTV_ConfigureBridgeIntent: invalid code path: intents has no slots'
-      const error = new Error(errorMessage)
-      Common.Debug.debugErrorWithStack(error)
-      throw error
-    }
-
-    if ((typeof intentRequest.intent.slots.ipAddressA === 'undefined') ||
-        (typeof intentRequest.intent.slots.ipAddressB === 'undefined') ||
-        (typeof intentRequest.intent.slots.ipAddressC === 'undefined') ||
-        (typeof intentRequest.intent.slots.ipAddressD === 'undefined') ||
-        (typeof intentRequest.intent.slots.hostnameIndex === 'undefined')) {
-      const errorMessage = 'LGTV_ConfigureBridgeIntent: invalid code path: missing slot(s)'
-      const error = new Error(errorMessage)
-      Common.Debug.debugErrorWithStack(error)
-      throw error
-    }
-
     const dialogState: ASKModel.DialogState = ASKRequestEnvelope.getDialogState(handlerInput.requestEnvelope)
     Common.Debug.debug(`dialogState: ${dialogState}`)
 
@@ -68,6 +72,7 @@ const ConfigureBridgeIntentHandler = {
     const ipAddressBString: String | undefined = ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressB')
     const ipAddressCString: String | undefined = ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressC')
     const ipAddressDString: String | undefined = ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressD')
+    const ipAddressValidString: String | undefined = ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'ipAddressValid')
     const hostnameIndexString: String | undefined = ASKRequestEnvelope.getSlotValue(handlerInput.requestEnvelope, 'hostnameIndex')
     Common.Debug.debug(`(dirty) address: ${ipAddressAString}.${ipAddressBString}.${ipAddressCString}.${ipAddressDString}, hostnameIndex: ${hostnameIndexString}`)
 
@@ -116,6 +121,7 @@ const ConfigureBridgeIntentHandler = {
           .withShouldEndSession(true)
           .getResponse()
       }
+
       if ((!Number.isInteger(ipAddressA)) || (ipAddressA < 0) || (ipAddressA > 255)) {
         const speechOutput = 'The first octet was out of range. It must be between 0 and 255. Please start over.'
         return handlerInput.responseBuilder
@@ -144,6 +150,7 @@ const ConfigureBridgeIntentHandler = {
           .withShouldEndSession(true)
           .getResponse()
       }
+
       return handlerInput.responseBuilder
         .addDelegateDirective()
         .getResponse()
@@ -155,27 +162,9 @@ const ConfigureBridgeIntentHandler = {
           .addDelegateDirective()
           .getResponse()
       }
-      if ((!Number.isInteger(ipAddressA)) || (ipAddressA < 0) || (ipAddressA > 255)) {
-        const speechOutput = 'The first octet was out of range. It must be between 0 and 255.'
-        const reprompt = 'Please tell me the first octet again.'
-        return handlerInput.responseBuilder
-          .addElicitSlotDirective('ipAddressA')
-          .speak(speechOutput)
-          .reprompt(reprompt)
-          .getResponse()
-      }
       if (typeof ipAddressBString === 'undefined') {
         return handlerInput.responseBuilder
           .addDelegateDirective()
-          .getResponse()
-      }
-      if ((!Number.isInteger(ipAddressB)) || (ipAddressB < 0) || (ipAddressB > 255)) {
-        const speechOutput = 'The second octet was out of range. It must be between 0 and 255.'
-        const reprompt = 'Please tell me the second octet again.'
-        return handlerInput.responseBuilder
-          .addElicitSlotDirective('ipAddressB')
-          .speak(speechOutput)
-          .reprompt(reprompt)
           .getResponse()
       }
       if (typeof ipAddressCString === 'undefined') {
@@ -183,85 +172,44 @@ const ConfigureBridgeIntentHandler = {
           .addDelegateDirective()
           .getResponse()
       }
-      if ((!Number.isInteger(ipAddressC)) || (ipAddressC < 0) || (ipAddressC > 255)) {
-        const speechOutput = 'The third octet was out of range. It must be between 0 and 255.'
-        const reprompt = 'Please tell me the third octet again.'
-        return handlerInput.responseBuilder
-          .addElicitSlotDirective('ipAddressC')
-          .speak(speechOutput)
-          .reprompt(reprompt)
-          .getResponse()
-      }
       if (typeof ipAddressDString === 'undefined') {
         return handlerInput.responseBuilder
           .addDelegateDirective()
           .getResponse()
       }
-      if ((!Number.isInteger(ipAddressD)) || (ipAddressD < 0) || (ipAddressD > 255)) {
-        const speechOutput = 'The fourth octet was out of range. It must be between 0 and 255.'
-        const reprompt = 'Please tell me the fourth octet again.'
+      if (typeof ipAddressValidString === 'undefined') {
         return handlerInput.responseBuilder
-          .addElicitSlotDirective('ipAddressD')
-          .speak(speechOutput)
-          .reprompt(reprompt)
+          .addDelegateDirective()
+          .getResponse()
+      }
+
+      if (ipAddressValidString === 'no') {
+        return handlerInput.responseBuilder
+          .speak('Oops. Let\'s start over.')
+          .withShouldEndSession(true)
           .getResponse()
       }
 
       if (typeof hostnameIndexString === 'undefined') {
-        sessionAttributes.ipAddress = `${ipAddressA}.${ipAddressB}.${ipAddressC}.${ipAddressD}`
-        Reflect.deleteProperty(sessionAttributes, 'hostnames')
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
-        Common.Debug.debug(`LGTV_ConfigureBridgeIntent: IPv4 address is ${sessionAttributes.hostname}`)
+        let cardContent
         try {
-          sessionAttributes.hostnames = await getHostnames(sessionAttributes.ipAddress, 25392)
+          cardContent = await creatHostnamesSimpleCardContent(handlerInput)
         } catch (error) {
-          Common.Debug.debug(`LGTV_ConfigureBridgeIntent: cannot connect to IPv4 address '${sessionAttributes.ipAddress}'`)
-          if (error instanceof Error) {
-            const cardTitle = `${Common.constants.application.name.pretty} Error`
-            const cardContent = 'I heard the I.P. Address: ' +
-                                `${ipAddressAString}.${ipAddressBString}.${ipAddressBString}.${ipAddressBString}`
-            const speechOutput = 'I had a problem connecting to the I.P. address. ' +
-                                 'A card in the Alexa App shows more.'
-            return handlerInput.responseBuilder
-              .withSimpleCard(cardTitle, cardContent)
-              .speak(speechOutput)
-              .getResponse()
-          } else {
-            const cardTitle = `${Common.constants.application.name.pretty} Error`
-            const cardContent = 'Unknown: Unknown'
-            const speechOutput = 'I had a problem connecting to the I.P. address.' +
-                                 'A card in the Alexa App shows more.'
-            return handlerInput.responseBuilder
-              .withSimpleCard(cardTitle, cardContent)
-              .speak(speechOutput)
-              .getResponse()
-          }
+          return handlerInput.responseBuilder
+            .speak(`${(error as Error).message} Please check your bridge installation and start over.`)
+            .withShouldEndSession(true)
+            .getResponse()
         }
-        Common.Debug.debug(`LGTV_ConfigureBridgeIntent: bridge FQDNs: ${sessionAttributes.hostnames}`)
+
         const cardTitle = 'Bridge  Hostname Configuration'
-        let cardContent: string = ''
-        let index = 0
-        while (index < sessionAttributes.hostnames.length) {
-          cardContent += `${index}: ${sessionAttributes.hostnames[index]}\n`
-          index += 1
-        }
-        cardContent += `\n${index}: My bridge is not in the list of hostnames.`
-        index += 1
-        cardContent += `\n${index}: My IP address is not '${sessionAttributes.ipAddress}'.`
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
         Common.Debug.debug(`LGTV_ConfigureBridgeIntent: bridge FQDN prompt: ${cardContent}`)
-        const speechOutput = 'Thank you. ' +
-            'Now, could you look at the card in your Alexa App and ' +
-            'tell me the number next to your bridge\'s hostname?'
         return handlerInput.responseBuilder
-          .speak(speechOutput)
           .withSimpleCard(cardTitle, cardContent)
-          .addElicitSlotDirective('hostnameIndex')
+          .addDelegateDirective()
           .getResponse()
       }
 
       const hostnameIndex: Number = Number(hostnameIndexString)
-
       if ((!Number.isInteger(hostnameIndex)) ||
           (hostnameIndex >= sessionAttributes.hostnames.length + 2) ||
           (hostnameIndex < 0)) {
@@ -276,12 +224,13 @@ const ConfigureBridgeIntentHandler = {
       }
       if (hostnameIndex === (sessionAttributes.hostnames.length + 1)) {
         return handlerInput.responseBuilder
-          .speak('I\'m sorry. I misheard your bridge\'s I.P. address. Maybe we could try again.')
+          .speak('I\'m sorry. I misheard your bridge\'s I.P. address. Will we need to start over.')
+          .withShouldEndSession(true)
           .getResponse()
       }
       if (hostnameIndex === sessionAttributes.hostnames.length) {
         return handlerInput.responseBuilder
-          .speak('I could not discover your bridge\'s hostname.')
+          .speak('I\'m sorry. I could not discover your bridge\'s hostname.')
           .getResponse()
       }
       if ((handlerInput.requestEnvelope.request as ASKModel.IntentRequest).intent.confirmationStatus === 'NONE') {
