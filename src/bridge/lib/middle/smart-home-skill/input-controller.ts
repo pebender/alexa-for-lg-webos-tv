98b5d9670d609ae2a452a1853180ce7a1f47ff81
Paul Bender <pebender@gmail.com>
2022-05-07 17:27:01 -0700

    Refined Alexa.InputController capability reporting.

--- src/bridge/lib/middle/smart-home-skill/input-controller.ts
+++ src/bridge/lib/middle/smart-home-skill/input-controller.ts
@@ -2,85 +2,231 @@ import * as Common from "../../../../common";
 import { BackendControl } from "../../backend";
 import LGTV from "lgtv2";
 
-const alexaToLGTV: { [key: string]: string } = {
-  "HDMI 1": "HDMI_1",
-  "HDMI 2": "HDMI_2",
-  "HDMI 3": "HDMI_3",
-  "HDMI 4": "HDMI_4",
-  "HDMI 5": "HDMI_5",
-  "HDMI 6": "HDMI_6",
-  "HDMI 7": "HDMI_7",
-  "HDMI 8": "HDMI_8",
-  "HDMI 9": "HDMI_9",
-  "HDMI 10": "HDMI_10",
-  "INPUT 1": "HDMI_1",
-  "INPUT 2": "HDMI_2",
-  "INPUT 3": "HDMI_3",
-  "INPUT 4": "HDMI_4",
-  "INPUT 5": "HDMI_5",
-  "INPUT 6": "HDMI_6",
-  "INPUT 7": "HDMI_7",
-  "INPUT 8": "HDMI_8",
-  "INPUT 9": "HDMI_9",
-  "INPUT 10": "HDMI_10",
-  "VIDEO 1": "AV_1",
-  "VIDEO 2": "AV_2",
-  "VIDEO 3": "AV_3",
+// The list if Alexa.InputController inputs from
+// <https://developer.amazon.com/en-US/docs/alexa/device-apis/alexa-inputcontroller.html>.
+// To determine mapping, the software compares these values with LG webOS TV
+// external input ids and labels. It uses uppercase values for the comparison.
+// It does the comparisons with and without spaces replaced with '_'. In
+// addition, it repeats the same comparisons after applying the renaming of
+// Alexa inputs using 'alexaInputRenameList'. If it fails to find any match,
+// then the software uses LGTV webOS TV external input id and label as Alexa
+// inputs.
+//
+// The software does not check to be sure that
+//   - there are no duplicate LG webOS TV external input ids.
+//   - there are no duplicate LG webOS TV external input labels.
+//   - there are no duplicates between the LG webOS TV ids and inputs.
+const alexaInputList: string[] = [
+  "AUX 1",
+  "AUX 2",
+  "AUX 3",
+  "AUX 4",
+  "AUX 5",
+  "AUX 6",
+  "AUX 7",
+  "BLURAY",
+  "CABLE",
+  "CD",
+  "COAX 1",
+  "COAX 2",
+  "COMPOSITE 1",
+  "DVD",
+  "GAME",
+  "HD RADIO",
+  "HDMI 1",
+  "HDMI 2",
+  "HDMI 3",
+  "HDMI 4",
+  "HDMI 5",
+  "HDMI 6",
+  "HDMI 7",
+  "HDMI 8",
+  "HDMI 9",
+  "HDMI 10",
+  "HDMI ARC",
+  "INPUT 1",
+  "INPUT 2",
+  "INPUT 3",
+  "INPUT 4",
+  "INPUT 5",
+  "INPUT 6",
+  "INPUT 7",
+  "INPUT 8",
+  "INPUT 9",
+  "INPUT 10",
+  "IPOD",
+  "LINE 1",
+  "LINE 2",
+  "LINE 3",
+  "LINE 4",
+  "LINE 5",
+  "LINE 6",
+  "LINE 7",
+  "MEDIA PLAYER",
+  "OPTICAL 1",
+  "OPTICAL 2",
+  "PHONO",
+  "PLAYSTATION",
+  "PLAYSTATION 3",
+  "PLAYSTATION 4",
+  "SATELLITE",
+  "SMARTCAST",
+  "TUNER",
+  "TV",
+  "USB DAC",
+  "VIDEO 1",
+  "VIDEO 2",
+  "VIDEO 3",
+  "XBOX",
+];
+
+// Non-trivial mappings between Alexa inputs and LGTV webOS TV ids.
+const lgtvExternalInputRenameList: { [key: string]: string } = {
+  "AV 1": "VIDEO 1",
+  "AV 2": "VIDEO 2",
+  "AV 3": "VIDEO 3",
 };
-const lgtvToAlexa: { [key: string]: string } = {
-  HDMI_1: "HDMI 1",
-  HDMI_2: "HDMI 2",
-  HDMI_3: "HDMI 3",
-  HDMI_4: "HDMI 4",
-  HDMI_5: "HDMI 5",
-  HDMI_6: "HDMI 6",
-  HDMI_7: "HDMI 7",
-  HDMI_8: "HDMI 8",
-  HDMI_9: "HDMI 9",
-  HDMI_10: "HDMI 10",
-  AV_1: "VIDEO 1",
-  AV_2: "VIDEO 2",
-  AV_3: "VIDEO 3",
+
+type ExternalInput = {
+  id: string;
+  label: string;
+  appId: string;
+  device: LGTV.ResponseExternalInputListDevice;
 };
+
+type ExternalInputMap = {
+  [input: string]: ExternalInput;
+};
+
+async function getAlexaToLGTV(
+  backendControl: BackendControl
+): Promise<ExternalInputMap> {
+  if (backendControl.getPowerState() === "OFF") {
+    return {};
+  }
+
+  const externalInputList: ExternalInput[] = [];
+  const lgtvResponse: LGTV.Response = await backendControl.lgtvCommand({
+    uri: "ssap://tv/getExternalInputList",
+  });
+  const devices = (lgtvResponse as LGTV.ResponseExternalInputList).devices;
+  devices.forEach((device) => {
+    const id = device.id.toUpperCase().replace(/_/g, " ");
+    const label = device.label.toUpperCase().replace(/_/g, " ");
+    const appId = device.appId;
+    externalInputList.push({
+      id,
+      label,
+      appId,
+      device,
+    });
+  });
+
+  const alexaInputMap: ExternalInputMap = {};
+
+  //
+  // First map Alexa inputs that are a match for external input labels.
+  //
+  const externalInputLabelMap: ExternalInputMap = {};
+  // Create an external input map using the label as the key.
+  externalInputList.forEach((input) => {
+    if (typeof externalInputLabelMap[input.label] === "undefined") {
+      externalInputLabelMap[input.label] = input;
+    }
+  });
+  //
+  alexaInputList.forEach((input) => {
+    if (typeof externalInputLabelMap[input] !== "undefined") {
+      alexaInputMap[input] = externalInputLabelMap[input];
+    }
+  });
+
+  //
+  // Second map Alexa inputs that re a match for external input ids.
+  // Skip any external inputs that have already been mapped by label.
+  //
+  const externalInputIdlMap: ExternalInputMap = {};
+  // Create an external input map using the id as the key.
+  externalInputList.forEach((input) => {
+    if (typeof externalInputIdlMap[input.id] === "undefined") {
+      externalInputIdlMap[input.id] = input;
+    }
+  });
+  // Add to the external input map using the renamed id as the key.
+  externalInputList.forEach((input) => {
+    const idRenamed = lgtvExternalInputRenameList[input.id];
+    if (
+      typeof idRenamed !== "undefined" &&
+      typeof externalInputIdlMap[idRenamed] === "undefined"
+    ) {
+      externalInputIdlMap[idRenamed] = input;
+    }
+  });
+
+  // Delete from external input map any that were already in the Alexa map using their label.
+  Object.keys(alexaInputMap).forEach((key) => {
+    const id = alexaInputMap[key].id;
+    if (typeof externalInputIdlMap[id] !== "undefined") {
+      delete externalInputIdlMap[id];
+    }
+    const idRenamed = lgtvExternalInputRenameList[id];
+    if (typeof externalInputIdlMap[idRenamed] !== "undefined") {
+      delete externalInputIdlMap[idRenamed];
+    }
+  });
+
+  //
+  alexaInputList.forEach((input) => {
+    if (
+      typeof alexaInputMap[input] === "undefined" &&
+      typeof externalInputIdlMap[input] !== "undefined"
+    ) {
+      alexaInputMap[input] = externalInputIdlMap[input];
+    }
+  });
+
+  return alexaInputMap;
+}
 
 function capabilities(
   backendControl: BackendControl
 ): Promise<Common.SHS.Event.Payload.Endpoint.Capability>[] {
-  return [
-    Common.SHS.Response.buildPayloadEndpointCapability({
-      namespace: "Alexa.InputController",
-      propertyNames: ["input"],
-    }),
-  ];
+  async function getInputCapability(
+    backendControl: BackendControl
+  ): Promise<Common.SHS.Event.Payload.Endpoint.Capability> {
+    const inputs: { name: string; friendlyNames: string[] }[] = [];
+    const alexaToLGTV = await getAlexaToLGTV(backendControl);
+    Object.keys(alexaToLGTV).forEach((alexaInput) => {
+      if (alexaToLGTV[alexaInput].label === alexaInput) {
+        inputs.push({
+          name: alexaInput,
+          friendlyNames: [],
+        });
+      } else {
+        inputs.push({
+          name: alexaInput,
+          friendlyNames: [alexaToLGTV[alexaInput].label],
+        });
+      }
+    });
+    const capability = await Common.SHS.Response.buildPayloadEndpointCapability(
+      {
+        namespace: "Alexa.InputController",
+        propertyNames: ["input"],
+      }
+    );
+    capability.inputs = inputs;
+    return capability;
+  }
+
+  return [getInputCapability(backendControl)];
 }
 
 function states(
   backendControl: BackendControl
 ): Promise<Common.SHS.Context.Property>[] {
   async function value(): Promise<string | null> {
-    async function getExternalInputList(): Promise<
-      LGTV.ResponseExternalInputListDevice[]
-    > {
-      if (backendControl.getPowerState() === "OFF") {
-        return [];
-      }
-
-      const lgtvRequest: LGTV.Request = {
-        uri: "ssap://tv/getExternalInputList",
-      };
-      const lgtvResponse: LGTV.ResponseExternalInputList =
-        (await backendControl.lgtvCommand(
-          lgtvRequest
-        )) as LGTV.ResponseExternalInputList;
-      if (typeof lgtvResponse.devices === "undefined") {
-        return [];
-      }
-      if (typeof lgtvResponse.devices === "undefined") {
-        throw new Error("invalid LGTVResponse message");
-      }
-      return lgtvResponse.devices;
-    }
-
+    const alexaToLGTV = await getAlexaToLGTV(backendControl);
     async function getInput(): Promise<string | null> {
       if (backendControl.getPowerState() === "OFF") {
         return null;
@@ -97,25 +243,17 @@ function states(
       }
       return lgtvResponse.appId;
     }
-    const [inputList, appId] = await Promise.all([
-      getExternalInputList(),
-      getInput(),
-    ]);
-
-    let input: string | null = null;
-    if (appId !== null) {
-      inputList.forEach((item): void => {
-        if (item.appId === appId) {
-          input = item.id;
-          if (typeof lgtvToAlexa.input !== "undefined") {
-            input = lgtvToAlexa[input];
-          } else {
-            input = input.replace("_", " ");
-          }
-        }
-      });
+    const appId = await getInput();
+    if (appId === null) {
+      return null;
     }
-    return input;
+    const alexaInput = Object.keys(alexaToLGTV).find(
+      (item) => alexaToLGTV[item].appId === appId
+    );
+    if (typeof alexaInput === "undefined") {
+      return null;
+    }
+    return alexaInput;
   }
 
   const inputState = Common.SHS.Response.buildContextProperty({
@@ -130,55 +268,11 @@ async function selectInputHandler(
   alexaRequest: Common.SHS.Request,
   backendControl: BackendControl
 ): Promise<Common.SHS.ResponseWrapper> {
-  async function getExternalInputList(): Promise<
-    LGTV.ResponseExternalInputListDevice[]
-  > {
-    const lgtvRequest: LGTV.Request = {
-      uri: "ssap://tv/getExternalInputList",
-    };
-    const lgtvResponse: LGTV.ResponseExternalInputList =
-      (await backendControl.lgtvCommand(
-        lgtvRequest
-      )) as LGTV.ResponseExternalInputList;
-    if (typeof lgtvResponse.devices === "undefined") {
-      return [];
-    }
-    return lgtvResponse.devices;
-  }
-
   function getInput(): string {
     if (typeof alexaRequest.directive.payload.input !== "string") {
       return "";
     }
     return alexaRequest.directive.payload.input.toUpperCase();
-  }
-
-  function mapInput(
-    inputList: LGTV.ResponseExternalInputListDevice[],
-    inputItem: string
-  ): string | null {
-    let input = inputItem;
-    if (typeof alexaToLGTV[inputItem] !== "undefined") {
-      input = alexaToLGTV[inputItem];
-    }
-    let device: string | null = null;
-    inputList.forEach((value): void => {
-      const id = value.id.toUpperCase();
-      const label = value.label.toUpperCase();
-      if (id === input) {
-        device = id;
-      }
-      if (id === input.replace(/ /g, "_")) {
-        device = id;
-      }
-      if (label === input) {
-        device = id;
-      }
-      if (label === input.replace(/ /g, "_")) {
-        device = id;
-      }
-    });
-    return device;
   }
 
   async function setExternalInput(
@@ -198,10 +292,15 @@ async function selectInputHandler(
     return Common.SHS.ResponseWrapper.buildAlexaResponse(alexaRequest);
   }
 
-  const lgtvInputList = await getExternalInputList();
-  const lgtvAppId = await getInput();
-  const lgtvInput = mapInput(lgtvInputList, lgtvAppId);
-  return setExternalInput(lgtvInput);
+  const alexaToLGTV = await getAlexaToLGTV(backendControl);
+  const alexaInput = await getInput();
+  let lgtvId: string | null;
+  if (typeof alexaToLGTV[alexaInput] === "undefined") {
+    lgtvId = null;
+  } else {
+    lgtvId = alexaToLGTV[alexaInput].device.id;
+  }
+  return setExternalInput(lgtvId);
 }
 
 function handler(
