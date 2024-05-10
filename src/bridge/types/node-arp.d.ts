d6c74225bad370ad00017062771059a132dedc0b
Paul Bender <pebender@gmail.com>
2024-05-10 13:17:57 -0700

    Minor cleanup.

--- src/bridge/types/node-arp.d.ts
+++ src/bridge/types/node-arp.d.ts
@@ -2,20 +2,20 @@ declare module "node-arp" {
   export function getMAC(
     ipaddress: string,
     cb: (found: boolean, result: string) => void,
-  );
+  ): void;
 
   export function getMACLinux(
     ipaddress: string,
     cb: (found: boolean, result: string) => void,
-  );
+  ): void;
 
   export function getMACWindows(
     ipaddress: string,
     cb: (found: boolean, result: string) => void,
-  );
+  ): void;
 
   export function getMACMac(
     ipaddress: string,
     cb: (found: boolean, result: string) => void,
-  );
+  ): void;
 }
