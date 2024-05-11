bbdbd3dd3020ff2960df69595afac7bf06b42be1
Paul Bender <pebender@gmail.com>
2024-05-11 16:37:52 -0700

    Problems found with @typescript-eslint/no-unsafe-return already fixed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,6 @@ module.exports = {
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
-    "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/only-throw-error": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
   },
