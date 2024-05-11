9d4b623fccbc23121183989f0e24a8c8c65a8b97
Paul Bender <pebender@gmail.com>
2024-05-10 17:40:39 -0700

    Problems found by @typescript-eslint/no-unsafe-call we already fixed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,6 @@ module.exports = {
     "@typescript-eslint/no-unnecessary-type-assertion": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/only-throw-error": "off",
